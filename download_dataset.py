ddf1773a6d58609f6993b25fd2bd959355ae0cdc
nrjkumar <neerajk764@gmail.com>
2025-06-15 17:34:20 -0400

    Fix-Download

--- download_dataset.py
+++ download_dataset.py
@@ -1,8 +1,4 @@
-from huggingface_hub import hf_hub_download
-hf_hub_download(
+from huggingface_hub import snapshot_download
+snapshot_download(
     repo_id="allenai/peS2o",
-    subfolder = "v3",
-    repo_type="dataset",
-    cache_dir = "/network/projects/living-review",
-    filename="train-*"
-)
+   allow_patterns="*.zst")
