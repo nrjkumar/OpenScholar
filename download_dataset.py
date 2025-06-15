53e1713370b4420c5471bb1f87a41ba985da30c2
nrjkumar <neerajk764@gmail.com>
2025-06-15 19:31:42 -0400

    Fix

--- download_dataset.py
+++ download_dataset.py
@@ -2,4 +2,5 @@ from huggingface_hub import snapshot_download
 snapshot_download(
     repo_id="allenai/peS2o",
     repo_type ="dataset",
-   allow_patterns="*.zst")
+   allow_patterns="*.zst",
+   cache_dir="/network/projects/living-review")
