bb4d287daac5377530518cf6fa34c42c03cf4b1b
nrjkumar <neerajk764@gmail.com>
2025-06-15 16:52:56 -0400

    Updates

--- download_dataset.py
+++ download_dataset.py
@@ -2,5 +2,6 @@ from huggingface_hub import snapshot_download
 snapshot_download(
     repo_id="allenai/peS2o",
     allow_patterns="/data/v3/*",
-    repo_type="dataset"
+    repo_type="dataset",
+    cache_dir = "/network/projects/living-review"
 )
