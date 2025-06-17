3dbe206e1d7d94fbe18156c18b4bd5668b6788c6
nrjkumar <neerajk764@gmail.com>
2025-06-17 19:38:02 -0400

    Update-time

--- convert_json.py
+++ convert_json.py
@@ -1,12 +1,13 @@
 from datasets import load_dataset
 from tqdm import tqdm
+import time
 
 ds = load_dataset("OpenSciLM/OpenScholar-DataStore-V3")
 
 for split, dataset in ds.items():
     files = dataset.to_json(f"my-dataset-{split}.jsonl")
     files.save_to_disk('$SCRATCH/OpenScholar/data/json')
-    for i in files:
-        tqdm.write(i)
+    for i in tqdm (range (len(files))): #for i in files:
+        time.sleep(0.1)
     
     print(f"Saved {len(files)} files to {files}")
