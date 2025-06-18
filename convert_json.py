b03df9716469ddd670dc41876756b2b8b303cacd
nrjkumar <neerajk764@gmail.com>
2025-06-18 15:04:19 -0400

    update-download file

--- convert_json.py
+++ convert_json.py
@@ -6,7 +6,7 @@ ds = load_dataset("OpenSciLM/OpenScholar-DataStore-V3")
 
 for split, dataset in ds.items():
     #files = dataset.to_json(f"my-dataset-{split}.jsonl")
-    files = dataset.save_to_disk('$SCRATCH/OpenScholar/data/json')
+    files = dataset.load_from_disk('$SCRATCH/OpenScholar/data/json')
     for i in tqdm (range (len(files))): #for i in files:
         time.sleep(0.1)
     
--- download_dataset.py
+++ download_dataset.py
@@ -3,4 +3,5 @@ snapshot_download(
     repo_id="allenai/peS2o",
     repo_type ="dataset",
    allow_patterns="*.zst",
-   cache_dir="/network/projects/living-review")
+   #cache_dir="'/network/projects/living-review'"
+   local_dir = "$SLURMTMPDIR/dataset")
